70d4a0adaa4b8de226d2fa27ac260b413ca5c0bb
Kurtsley <73447098+Kurtsley@users.noreply.github.com>
2024-06-04 19:52:12 -0500

    basic list function

--- static/app.js
+++ static/app.js
@@ -1,2 +1,16 @@
 // Kurtsley 2024
 
+const listSection = document.querySelector(".list-section");
+
+for (let i = 0; i < 25; i++) {
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.id = "box" + i;
+
+    const listItem = document.createElement("li");
+    listItem.id = "item" + i;
+    listItem.classList.add("listItem");
+
+    listItem.appendChild(checkBox);
+    listSection.appendChild(listItem);
+}
